d7b8e3929f13d66d3fe1232ef237b4e2634c9324
b-sai <bsaishreyas@gmail.com>
2023-11-27 19:07:07 -0600

    Add ML tools and update image capture

--- flask_app/main.py
+++ flask_app/main.py
@@ -6,12 +6,22 @@ import io
 from utils import identify_object
 import spacy
 from dotenv import load_dotenv
-import replicate
+from transformers import BlipProcessor, BlipForQuestionAnswering
+from PIL import Image
+load_dotenv()
+print("Loading language tools")
+
 load_dotenv()
 nlp = spacy.load("en_core_web_sm")
 
 app = Flask(__name__)
 r = sr.Recognizer()
+
+print("Loading ML tools")
+is_video_on = True
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-vqa-base")
+model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base")
 
 counter = 0
 @app.route('/')
@@ -34,15 +44,14 @@ def generate_frames():
 def video():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-is_video_on = False
 
 @app.route('/capture')
 def capture():
     camera = cv2.VideoCapture(0)
     success, frame = camera.read()
     if success:
-        cv2.imwrite('C:/Users/bsais/Desktop/image.jpg', frame)  
-        return "Frame captured and saved successfully", 200
+        pil_image = Image.fromarray(frame)
+        return pil_image, 200
     else:
         return "Could not capture image", 500
 
@@ -51,26 +60,23 @@ def capture():
 def toggle_video():
     global is_video_on
     is_video_on = not is_video_on
+    print("in toggle!")
     return redirect(url_for('index'))
 
-def get_api_result(obj_to_be_found):
-    image_path = 'C:/Users/bsais/Desktop/image.jpg'
-    output = replicate.run(
-        "yorickvp/llava-13b:2facb4a474a0462c15041b78b1ad70952ea46b5ec6ad29583c0b29dbd4249591",
-        input={"image": open(image_path, "rb"),
-               'prompt': f'Please describe where {obj_to_be_found} is in this image'}
-    )
-    llava_response = "" 
-    for item in output:
-        llava_response+=item
-    return llava_response
+
+def get_api_result(image, question):
+    inputs = processor(image, question, return_tensors="pt")
+    out = model.generate(inputs["input_ids"],
+                         pixel_values=inputs["pixel_values"])
+    answer = processor.decode(out[0], skip_special_tokens=True)
+    return answer
 
 @app.route('/get_vqa_result', methods = ['POST'])
 def get_vqa_res():
-    # capture()
+    image, status = capture()
     object_to_be_found = request.json['voice_input']
-    print(object_to_be_found, "Object to be found")
-    ml_model_result = get_api_result(object_to_be_found)
+    print(object_to_be_found, " -- Object to be found")
+    ml_model_result = get_api_result(image, object_to_be_found)
     return jsonify({"vqa_response": ml_model_result})
 
 
@@ -82,8 +88,8 @@ def get_user_speech():
         audio = r.listen(source)
         text = r.recognize_google(audio)
         print(text)
-        obj_to_be_found = identify_object(nlp, text)
-        print(f"Recognized text: {obj_to_be_found}")
-    return jsonify({"obj_to_be_found": obj_to_be_found})
+        # obj_to_be_found = identify_object(nlp, text)
+        print(f"Recognized text: {text}")
+    return jsonify({"obj_to_be_found": text})
 if __name__ == '__main__':
     app.run(debug=True)
